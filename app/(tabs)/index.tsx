4d46b0092e0fdecbbb2af08db72d1f9460b6e574
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2025-04-13 18:34:50 +0800

    init comit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,80 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { useState } from "react";
+import { Button, Text, View, Image, StyleSheet } from "react-native";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import DrinkButton from "../components/DrinkButton";
+export default function Index() {
+  const [drinkedCount, setDrinkedCount] = useState(0);
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+  const onPressDrinkWater = () => {
+    setDrinkedCount(drinkedCount + 1);
+  };
 
-export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Today</Text>
+        <Text style={styles.subtitle}>{drinkedCount} / 8 glass of water</Text>
+      </View>
+
+      <View style={styles.centerSection}>
+        <View style={styles.imageContainer}>
+          <Image source={require('@/assets/images/water.png')} style={styles.image} />
+        </View>
+        <DrinkButton onPress={onPressDrinkWater}/>
+      </View>
+      <View style = {styles.footer}>
+        <Text style = {styles.footerText}>Drinking Streak</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  // Layout
+  container: {
+    flex: 1,
+    backgroundColor: "#008EE5",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+
+  // Header Section
+  header: {
+    flex: 0.5,
+    alignItems: "center",
+    paddingTop: hp("5%"),
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  title: {
+    fontSize: hp("5%"),
+    fontWeight: "bold",
+    color: "#fff",
   },
+  subtitle: {
+    fontSize: hp("3%"),
+    color: "#fff",
+  },
+    centerSection: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+
+  },
+
+  imageContainer: {
+    marginBottom: hp("5%"),
+  },
+  image: {
+    width: wp("50%"),
+    height: hp("25%"),
+    resizeMode: "contain",
+  },
+
+  footer: {
+    flex: 0.5,
+    backgroundColor: 'white'
+  },
+  footerText: {
+    padding: wp("1%"),
+    fontSize: hp("3%"),
+    fontWeight: 'bold'
+
+  }
 });
---
+++ app/components/DrinkButton.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+type Props = {
+    onPress: () => void;
+}
+const DrinkButton = ({onPress}: Props) => {
+  return (
+    <Pressable style={({ pressed }) => [
+        styles.container,
+        pressed && styles.pressedContainer,
+      ]}
+      onPress={onPress}>
+      <Text style={styles.title}> DRINK WATER</Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    width: wp('50%'),
+    height: hp('10%'),
+    borderRadius: 30
+  },
+  pressedContainer: {
+    backgroundColor: '#D3D3D3',
+  },
+  title: {
+    fontSize: hp('3%'),
+    fontWeight: 'bold',
+  },
+});
+
+export default DrinkButton;
